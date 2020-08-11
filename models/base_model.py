0d96352343023594843c522b7cefa5d0bde49eac
laucavv <laucavv23@gmail.com>
2020-08-11 22:23:24 +0000

    Base model updated method init changed

--- models/base_model.py
+++ models/base_model.py
@@ -12,23 +12,22 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
 
-    id = Column(String(60), nullable = False, primarykey = True)
+    id = Column(String(60), nullable = False, primary_key = True)
     created_at = Column(DateTime, nullable = False, default = datetime.utcnow())
     updated_at = Column(DateTime, nullable = False, default = datetime.utcnow())
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value_2 = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value_2)
+                elif not key == '__class__':
+                    setattr(self, key, value)
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
