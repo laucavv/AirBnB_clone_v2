0936da9bc96c61a0d01f93422b3c410c7239eefa
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-11 18:51:43 +0000

    corrected syntax

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- Script that prepares a MySQL server for a project.
 
-CREATE DATABASE IF NOT EXIST hbnb_dev_db;
-CREATE USER IF NOT EXIST 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
