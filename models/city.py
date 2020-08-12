bf564430abf262079c2d080a2f98aebf02eb66b0
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-12 01:08:24 +0000

    add sqlalchemy syntax

--- models/city.py
+++ models/city.py
@@ -6,5 +6,5 @@ from sqlalchemy import Column, String, ForeignKey
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    state_id = Column(String(60), nullable=False, ForeignKey='states.id'
+    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
     name = Column(String(128), nullable=False)
