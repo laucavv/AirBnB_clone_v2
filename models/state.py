42784907f8d8b3d37e35462d51241e9fb92a06f4
laucavv <laucavv23@gmail.com>
2020-08-12 04:42:55 +0000

    State updated * relationship whith city created * choose a type storage

--- models/state.py
+++ models/state.py
@@ -2,11 +2,26 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, String, 
+from sqlalchemy import Column, String,
 from sqlalchemy import relationship
+from os import getenv
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state',
+                              cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """Attrubute that returns the list of City."""
+            list_cities = []
+            all_cities = models.storage.all(City)
+            for city_o in all_cities:
+                if self.id == city_o.state_id:
+                    list_cities.append(city_o)
+            return list_cities
