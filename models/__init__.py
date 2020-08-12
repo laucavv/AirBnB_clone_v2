149e7caef22267a7ced37bf954dc77c3d5204ca3
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-12 22:44:07 +0000

    init, place, amenity update. __init__: import all classes. Place:	  Create a relationship many-to-many with amenity Amenity:  Relationship many-to-many with Place created

--- models/__init__.py
+++ models/__init__.py
@@ -2,9 +2,15 @@
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
-from models.city import City
+from models.base_model import BaseModel, Base
+from models.amenity import Amenity
+from models.review import Review
 from models.state import State
+from models.place import Place
+from models.city import City
+from models.user import User
 from os import getenv
+
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     storage = DBStorage()
--- models/place.py
+++ models/place.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship, backref
 from os import getenv
 
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id',
+                             String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True,
+                             nullable=False),
+                      Column('amenity_id',
+                             String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True,
+                             nullable=False))
+
+
 class Place(BaseModel, Base):
-    """ A place to stay """
+    """A place to stay."""
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -23,6 +37,9 @@ class Place(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship('Review', backref='place',
                                cascade="all, delete")
+        amenities = relationship('Amenity',
+                                 secondary=place_amenity,
+                                 viewonly=False)
     else:
         @property
         def reviews(self):
@@ -33,3 +50,19 @@ class Place(BaseModel, Base):
                 if self.id == reviews_o.place_id:
                     list_reviews.append(reviews_o)
             return list_reviews
+
+        @property
+        def amenities(self):
+            """Attribute that returns the list of Amenity instances"""
+            list_amenity = []
+            all_amenity = models.storage.all(Amenity)
+            for amenity_o in all_amenity.values():
+                if amenity_0.id in self.amenity_ids:
+                    list_amenity.append(amenity_o)
+            return list_amenity
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """Attribute that add an Amenity.id to amenity_ids"""
+            if obj.__class__.__name__ == 'Amenity':
+                self.amenity_ids.append(obj)
