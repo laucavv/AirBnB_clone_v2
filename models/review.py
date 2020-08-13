121bdcd965691bf91831402e35d79ca5cccef643
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-13 00:12:46 +0000

    corrected syntax error, import

--- models/review.py
+++ models/review.py
@@ -2,7 +2,7 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from os from getenv
+from os import getenv
 
 
 class Review(BaseModel, Base):
