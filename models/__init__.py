d212eee991512062cf8335224eb045b11952b49c
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-13 00:21:09 +0000

    unnecesary imports deleted

--- models/__init__.py
+++ models/__init__.py
@@ -2,13 +2,6 @@
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
-from models.base_model import BaseModel, Base
-from models.amenity import Amenity
-from models.review import Review
-from models.state import State
-from models.place import Place
-from models.city import City
-from models.user import User
 from os import getenv
 
 
