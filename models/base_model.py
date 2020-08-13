d1497b5f4d0be6b4e0348e82f4c5adf7d6402d88
laucavv <laucavv23@gmail.com>
2020-08-13 00:40:59 +0000

    condition deleted

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,6 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
         id = Column(String(60), nullable=False, primary_key=True)
         created_at = Column(DateTime,
                             nullable=False,
