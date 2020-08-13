494ee36791567c28887734610a5ae524ee124526
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-13 00:31:56 +0000

    define the tablename outside of the condition

--- models/city.py
+++ models/city.py
@@ -8,8 +8,8 @@ from os import getenv
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         places = relationship('Place', backref='cities',
--- models/state.py
+++ models/state.py
@@ -10,8 +10,8 @@ import models
 
 class State(BaseModel, Base):
     """ State class """
+    __tablename__ = 'states'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state',
                               cascade="all, delete")
