b7811b8ae81b9d072a2c5bbfdb26439140be7a01
laucavv <laucavv23@gmail.com>
2020-09-01 22:22:13 +0000

    fix pep8

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -11,16 +11,19 @@ def Hello():
     """ display Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def Hbnb():
     """display HBNB"""
     return 'HBNB'
+
 
 @app.route('/c/<text>')
 def C_is_fun(text):
     """display c is <text>"""
     new_t = text.replace("_", " ")
     return 'C {}'.format(new_t)
+
 
 @app.route('/python')
 @app.route('/python/text')
@@ -29,11 +32,12 @@ def Python_is_fun(text='is cool'):
     new_t = text.replace("_", " ")
     return 'Python {}'.format(new_t)
 
+
 @app.route('/number/<int:n>')
 def Is_integer(n):
     """ Is integer"""
     return '{} is a number'.format(n)
-    
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
