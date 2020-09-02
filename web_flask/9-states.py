7df1e3b96d2602557cb74c1fabf5b4d252e82c32
laucavv <laucavv23@gmail.com>
2020-09-02 05:38:12 +0000

    state id

---
+++ web_flask/9-states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application """
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def db_close(db):
+    """Close sessions """
+    storage.close()
+
+
+@app.route('/states/', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def list_cities(id=None):
+    """ List State """
+    states = None
+    states_all = storage.all(State)
+    state_id = "State.{}".format(id)
+    if state_id in states_all.keys():
+        satates = states_all[state_id]
+    return render_template('9-states.html', states=states,
+                           id=id, states_all=states_all)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
